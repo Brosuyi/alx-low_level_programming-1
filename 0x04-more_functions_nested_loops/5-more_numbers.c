a4624a4df38dd6d18c0eb6096c7dce523782d659
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-03-30 18:46:44 +0100

    added 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 to 9 followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+	}
+	_putchar('\n');
+}
