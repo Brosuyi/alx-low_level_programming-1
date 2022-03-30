25d8c41e339e9c898a68a7777d872f48879d4f67
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-03-30 17:12:27 +0100

    added 4-print_more_files.c

---
+++ 0x04-more_functions_nested_loops/4-print_more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9 followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	char i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+		_putchar(i);
+		}
+	}
+	_putchar('\n');
+}
