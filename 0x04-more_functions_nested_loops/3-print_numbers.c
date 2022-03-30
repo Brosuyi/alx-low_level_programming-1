f6641d57eed6ba9a995e2fd47aeb85888cce307b
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-03-30 16:10:27 +0100

    added 3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 to 9 followed by a new line
+ *
+ * Return: Always 0
+ */
+
+void print_numbers(void)
+{
+	for (i = 0, i < 10, i++)
+	{
+		print("%d\n", i)
+	}
+}
