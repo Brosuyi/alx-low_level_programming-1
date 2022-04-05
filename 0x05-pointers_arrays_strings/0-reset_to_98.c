0bbbfbefd71917da9d6af733e60ae455ad41b2e0
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-04-05 01:36:25 +0100

    added second task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,9 @@
 /**
  * reset_to_98 - updates the value a pointer points to to 98
  * @p: pointer that points to the value to be updated
- *
- * Return: Always 0.
  */
 
-int reset_to_98(int *p)
+void reset_to_98(int *p)
 {
 	*p = 98;
-
-	return (0);
 }
