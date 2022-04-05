7f65b74bfb12505c46aae22df70c57fc2e960839
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-04-05 13:46:30 +0100

    third task

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string whose length is to be returned
+ * @len: length of the string s
+ *
+ * Returns: length of string s.
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	for (len = 0; *s != '\0'; *s++)
+	{
+		len++;
+	}
+	return (len);
+}
