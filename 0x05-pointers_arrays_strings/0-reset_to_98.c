00b2f5113ec5ea1bc2be5c9d06e890d6ccab8be0
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-04-05 01:12:17 +0100

    added README.md

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * reset_to_98 - update the value a pointer points to to 98
+ * reset_to_98 - updates the value a pointer points to to 98
+ * @p: pointer that points to the value to be updated
  *
  * Return: Always 0.
  */
@@ -10,5 +11,5 @@ int reset_to_98(int *p)
 {
 	*p = 98;
 
-	return(0);
+	return (0);
 }
