66b2ad40d298b83c03abc55098565914f9539128
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-04-11 22:31:19 +0100

    added task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: the string to check
+ *
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @src: the first string
+ * @dest: the second string
+ * Return: dest string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length = _strlen(dest);
+	int i;
+
+	for (i = 0; *(src + i) != '\0' && n > 0; i++, n--)
+	{
+		*(dest + length + i) = *(src + i);
+	}
+	*(dest + length + i) = '\0';
+
+	return (dest);
+}
