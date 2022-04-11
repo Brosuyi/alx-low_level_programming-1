d2e52338ef7adcfc01f636c1944dd5fe9344d402
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-04-12 00:39:04 +0100

    added task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,41 +1,22 @@
 #include "main.h"
 
 /**
- * _strlen - length of a string
- * @s: the string to check
+ * _strncpy - copies a string
+ * @src: the string to copy
+ * @dest: destination string
+ * @n: bytes of src to use
  *
- * Return: length of the string
+ * Return: pointer to the destination string
  */
-
-int _strlen(char *s)
-{
-	int length = 0;
-
-	for (; *s != '\0'; s++)
-	{
-		length++;
-	}
-	return (length);
-}
-
-/**
- * _strncpy - copies a string to another string upto the nth element
- * @src: the first string
- * @dest: the second string
- * @n: number of bytes to get from src
- * Return: dest string
- */
-
 char *_strncpy(char *dest, char *src, int n)
 {
-	int length = _strlen(dest);
 	int i;
 
-	for (i = 0; *(src + i) != '\0' && n > 0; i++, n--)
-	{
-		*(dest + i) = *(src + i);
-	}
-	*(dest + i) = '\0';
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+
+	for (; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
