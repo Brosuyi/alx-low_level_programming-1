90914019891510256cc5d4ee453daeac8854c52e
MagentaPuppet <paul.gabriel@bison.howard.edu>
2022-04-12 04:38:24 +0100

    added task 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#if (x < 0)
-	#define ABS(x) ((x) * (-1))
-#else
-	#define ABS(x) (x)
-#endif /* ABS(x) */
-#endif /* FUNCTION_LIKE_MACRO_H */
+#define ABS(X) ((X) < 0 ? -(X) : (X))
+
+#endif
